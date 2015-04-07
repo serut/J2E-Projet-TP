810f24682044f02f0f4ca28de161606baf834b71
Serut <l.mieulet@gmail.com>
2015-04-07 10:48:10 +0200

    Ajoute la méthode de recherche de musée

--- grails-app/services/toulousemusee/MuseeService.groovy
+++ grails-app/services/toulousemusee/MuseeService.groovy
@@ -5,7 +5,20 @@ import grails.transaction.Transactional
 @Transactional
 class MuseeService {
 
-    def serviceMethod() {
-
+    def public searchMusee(String inNameMusee, int codePostal, String inAdresseMusee) {
+            def criteria = Musee.createCriteria()
+            List<Musee> result = criteria.list {
+                if (inNameMusee) {
+                    like("name", "%${inNameMusee}%")
+                }
+                if (codePostal) {
+                    eq("codePostal", "%${codePostal}%")
+                }
+                if (inAdresseMusee) {
+                    like("inAdresseMusee", "%${inAdresseMusee}%")
+                }
+            }
+            result
+        }
     }
 }
